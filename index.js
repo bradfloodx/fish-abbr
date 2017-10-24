e39b86f7c7e9a08a259af81bda6e38ea4f46ab2a
Bradley Flood <bradley.flood@foxsports.com.au>
2017-10-25 10:05:53 +1100

    tweak import logging

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const {exec} = require('child_process');
 const list = require('./abbr.js');
 
+// Setup
 const config = {shell: '/usr/local/bin/fish'};
-
 const logger = (error, stdout, stderr) => {
 	if (error)
-		return console.error(`exec error: ${error}`);
+		return console.error(`EXEC ERROR: ${error}`);
 
 	if (stdout)
-		return console.log(`stdout: ${stdout}`);
+		return console.log(`${stdout}`);
 
 	if (stderr)
-		return console.log(`stderr: ${stderr}`);
+		return console.log(`STDERROR: ${stderr}`);
 }
 
+// Import
 list.forEach((abbr) => {
-	if (abbr.length && abbr.charAt(0) !== '#') {
-		console.log(`Adding: ${abbr}`);
+	if (abbr.charAt(0) === '#' || abbr.length === 0) {
+		console.log(abbr);
+		return;
+	}
+	else {
+		console.log(abbr);
 		exec(abbr, config, logger);
+		return;
 	}
 });
 
-exec("echo 'Abbreviations Imported!'", config ,logger);
+// Voila!
+exec("echo '~~~ Done! ~~~'", config, logger);
+
