2e0dceff13ec46e2edecb026a20a0294385b57b3
Bradley Flood <bradley.flood@foxsports.com.au>
2018-01-02 14:04:25 +1100

    Add gcam + rename abbrevations.txt

--- abbr.js
+++ abbr.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const abbrDump = fs.readFileSync('./abbr.txt', 'utf8');
+const abbrDump = fs.readFileSync('./abbreviations.txt', 'utf8');
 const abbr = abbrDump.split('\n');
 
 module.exports = abbr;
